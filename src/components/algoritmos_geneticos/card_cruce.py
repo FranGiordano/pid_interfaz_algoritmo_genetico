9cfa9662b2363115cc3087e2e918a05d6f10d66f
Franco <francoomargiordano@hotmail.com>
2024-04-29 16:24:10 -0300

    fixed ratios

--- src/components/algoritmos_geneticos/card_cruce.py
+++ src/components/algoritmos_geneticos/card_cruce.py
@@ -42,23 +42,23 @@ def card_cruce(cruce):
         ]
         if bits_padre_1_izq is not None:
             for i in bits_padre_1_izq:
-                input_group_cruce1.append(dbc.Input(readonly=True, value=i, style={'width': '50px'}, className='p-2 text-success'))
+                input_group_cruce1.append(dbc.Input(readonly=True, value=i, className='p-2 text-success'))
             for i in bits_padre_2_der:
-                input_group_cruce1.append(dbc.Input(readonly=True, value=i, style={'width': '50px'}, className='p-2 text-primary'))
+                input_group_cruce1.append(dbc.Input(readonly=True, value=i, className='p-2 text-primary'))
 
         input_group_cruce2 = [
             dbc.InputGroupText("Bits Cruce 2")
         ]
         for i in bits_padre_2_izq:
-            input_group_cruce2.append(dbc.Input(readonly=True, value=i, style={'width': '50px'}, className='p-2 text-primary'))
+            input_group_cruce2.append(dbc.Input(readonly=True, value=i, className='p-2 text-primary'))
         for i in bits_padre_1_der:
-            input_group_cruce2.append(dbc.Input(readonly=True, value=i, style={'width': '50px'}, className='p-2 text-success'))
+            input_group_cruce2.append(dbc.Input(readonly=True, value=i, className='p-2 text-success'))
 
         div_hijo1 = [
 
             dbc.InputGroup([
                 dbc.InputGroupText("Punto de Corte"),
-                dbc.Input(readonly=True, value=round(punto_corte, 2), style={'width': '50px'}, className='p-2')
+                dbc.Input(readonly=True, value=round(punto_corte, 2), className='p-2')
             ], className="p-2"),
 
             dbc.InputGroup(input_group_cruce1, className="p-2"),
@@ -72,11 +72,9 @@ def card_cruce(cruce):
 
             dbc.InputGroup([
                 dbc.InputGroupText("Entre gen"),
-                dbc.Input(readonly=True, value=(1 if punto_corte < 0.33 else 2 if punto_corte < 0.66 else 3),
-                          style={'width': '50px'}, className='p-2'),
+                dbc.Input(readonly=True, value=(1 if punto_corte < 0.33 else 2 if punto_corte < 0.66 else 3), className='p-2'),
                 dbc.InputGroupText("y gen"),
-                dbc.Input(readonly=True, value=(2 if punto_corte < 0.33 else 3 if punto_corte < 0.66 else 4),
-                          style={'width': '50px'}, className='p-2'),
+                dbc.Input(readonly=True, value=(2 if punto_corte < 0.33 else 3 if punto_corte < 0.66 else 4), className='p-2'),
             ], className="p-2"),
 
             dbc.InputGroup(input_group_cruce2, className="p-2"),
@@ -95,21 +93,21 @@ def card_cruce(cruce):
         html.Div([
 
             dbc.InputGroup([
-                dbc.InputGroupText("Probabilidad aleatoria Padre 1"),
-                dbc.Input(readonly=True, value=round(prob_ind_1, 2), style={'width': '50px'}, className='p-2')
+                dbc.InputGroupText("Prob Padre 1"),
+                dbc.Input(readonly=True, value=round(prob_ind_1, 2), className='p-2')
             ], className="mr-auto p-2"),
 
             dbc.InputGroup([
                 dbc.InputGroupText("Individuo"),
-                dbc.Input(readonly=True, value=nro_ind_padre1, style={'width': '50px'}, className='p-2')
+                dbc.Input(readonly=True, value=nro_ind_padre1, className='p-2')
             ], className="mr-auto p-2"),
 
             dbc.InputGroup([
-                dbc.InputGroupText("Bits Padre 1"),
-                dbc.Input(readonly=True, value=bits_individuo_padre1[0], style={'width': '50px'}, className='p-2 text-success'),
-                dbc.Input(readonly=True, value=bits_individuo_padre1[1], style={'width': '50px'}, className='p-2 text-success'),
-                dbc.Input(readonly=True, value=bits_individuo_padre1[2], style={'width': '50px'}, className='p-2 text-success'),
-                dbc.Input(readonly=True, value=bits_individuo_padre1[3], style={'width': '50px'}, className='p-2 text-success')
+                dbc.InputGroupText("Bits"),
+                dbc.Input(readonly=True, value=bits_individuo_padre1[0], className='p-2 text-success'),
+                dbc.Input(readonly=True, value=bits_individuo_padre1[1], className='p-2 text-success'),
+                dbc.Input(readonly=True, value=bits_individuo_padre1[2], className='p-2 text-success'),
+                dbc.Input(readonly=True, value=bits_individuo_padre1[3], className='p-2 text-success')
             ], className="mr-auto p-2"),
 
         ], className='d-flex'),
@@ -117,21 +115,21 @@ def card_cruce(cruce):
         html.Div([
 
             dbc.InputGroup([
-                dbc.InputGroupText("Probabilidad aleatoria Padre 2"),
-                dbc.Input(readonly=True, value=round(prob_ind_2, 2), style={'width': '50px'}, className='p-2')
+                dbc.InputGroupText("Prob Padre 2"),
+                dbc.Input(readonly=True, value=round(prob_ind_2, 2), className='p-2')
             ], className="mr-auto p-2"),
 
             dbc.InputGroup([
                 dbc.InputGroupText("Individuo"),
-                dbc.Input(readonly=True, value=nro_ind_padre2, style={'width': '50px'}, className='p-2')
+                dbc.Input(readonly=True, value=nro_ind_padre2, className='p-2')
             ], className="mr-auto p-2"),
 
             dbc.InputGroup([
-                dbc.InputGroupText("Bits Padre 2"),
-                dbc.Input(readonly=True, value=bits_individuo_padre2[0], style={'width': '50px'}, className='p-2 text-primary'),
-                dbc.Input(readonly=True, value=bits_individuo_padre2[1], style={'width': '50px'}, className='p-2 text-primary'),
-                dbc.Input(readonly=True, value=bits_individuo_padre2[2], style={'width': '50px'}, className='p-2 text-primary'),
-                dbc.Input(readonly=True, value=bits_individuo_padre2[3], style={'width': '50px'}, className='p-2 text-primary')
+                dbc.InputGroupText("Bits"),
+                dbc.Input(readonly=True, value=bits_individuo_padre2[0], className='p-2 text-primary'),
+                dbc.Input(readonly=True, value=bits_individuo_padre2[1], className='p-2 text-primary'),
+                dbc.Input(readonly=True, value=bits_individuo_padre2[2], className='p-2 text-primary'),
+                dbc.Input(readonly=True, value=bits_individuo_padre2[3], className='p-2 text-primary')
             ], className="mr-auto p-2"),
 
         ], className='d-flex'),
@@ -143,15 +141,15 @@ def card_cruce(cruce):
         html.Div([
 
             dbc.InputGroup([
-                dbc.InputGroupText("Probabilidad de Cruce"),
-                dbc.Input(readonly=True, value=round(prob_cruce, 2), style={'width': '50px'}, className='p-2'),
+                dbc.InputGroupText("Prob de Cruce"),
+                dbc.Input(readonly=True, value=round(prob_cruce, 2), className='p-2'),
                 dbc.InputGroupText('> <'),
-                dbc.Input(readonly=True, value=round(probabilidad_cruce, 2), style={'width': '50px'}, className='p-2')
+                dbc.Input(readonly=True, value=round(probabilidad_cruce, 2), className='p-2')
             ], className="mr-auto p-2"),
 
             dbc.InputGroup([
                 dbc.InputGroupText("¿Cruzan?"),
-                dbc.Input(readonly=True, value=('SI' if cruzo else 'NO'), style={'width': '50px'}, className='p-2')
+                dbc.Input(readonly=True, value=('SI' if cruzo else 'NO'), className='p-2')
             ], className="mr-auto p-2"),
 
             dbc.InputGroup([
@@ -169,31 +167,31 @@ def card_cruce(cruce):
         html.Div([
 
             dbc.InputGroup([
-                dbc.InputGroupText("Prob de Mutación Hijo 1"),
-                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_1[0], 2), style={'width': '50px'},
+                dbc.InputGroupText("Prob Mutación Hijo 1"),
+                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_1[0], 2),
                           className='p-2'),
-                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_1[1], 2), style={'width': '50px'},
+                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_1[1], 2),
                           className='p-2'),
-                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_1[2], 2), style={'width': '50px'},
+                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_1[2], 2),
                           className='p-2'),
-                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_1[3], 2), style={'width': '50px'},
+                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_1[3], 2),
                           className='p-2'),
                 dbc.InputGroupText('> <'),
-                dbc.Input(readonly=True, value=round(probabilidad_mutacion, 2), style={'width': '50px'}, className='p-2')
+                dbc.Input(readonly=True, value=round(probabilidad_mutacion, 2), className='p-2')
             ], className="mr-auto p-2"),
 
             dbc.InputGroup([
                 dbc.InputGroupText("Bits Hijo 1"),
-                dbc.Input(readonly=True, value=bits_individuo_hijo1[0], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo1[0],
                           className=f'p-2 {'text-danger' if probabilidad_mutaciones_1[0] < probabilidad_mutacion else ""}'),
-                dbc.Input(readonly=True, value=bits_individuo_hijo1[1], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo1[1],
                           className=f'p-2 {'text-danger' if probabilidad_mutaciones_1[1] < probabilidad_mutacion else ""}'),
-                dbc.Input(readonly=True, value=bits_individuo_hijo1[2], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo1[2],
                           className=f'p-2 {'text-danger' if probabilidad_mutaciones_1[2] < probabilidad_mutacion else ""}'),
-                dbc.Input(readonly=True, value=bits_individuo_hijo1[3], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo1[3],
                           className=f'p-2 {'text-danger' if probabilidad_mutaciones_1[3] < probabilidad_mutacion else ""}'),
                 dbc.InputGroupText('Peso'),
-                dbc.Input(readonly=True, value=peso_hijo1, style={'width': '50px'}, className=f'p-2 {"text-success" if hijo1_vivo else "text-danger"}')
+                dbc.Input(readonly=True, value=peso_hijo1, className=f'p-2 {"text-success" if hijo1_vivo else "text-danger"}')
             ], className="mr-auto p-2"),
 
         ], className='d-flex'),
@@ -201,32 +199,32 @@ def card_cruce(cruce):
         html.Div([
 
             dbc.InputGroup([
-                dbc.InputGroupText("Prob de Mutación Hijo 2"),
-                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_2[0], 2), style={'width': '50px'},
+                dbc.InputGroupText("Prob Mutación Hijo 2"),
+                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_2[0], 2),
                           className='p-2'),
-                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_2[1], 2), style={'width': '50px'},
+                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_2[1], 2),
                           className='p-2'),
-                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_2[2], 2), style={'width': '50px'},
+                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_2[2], 2),
                           className='p-2'),
-                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_2[3], 2), style={'width': '50px'},
+                dbc.Input(readonly=True, value=round(probabilidad_mutaciones_2[3], 2),
                           className='p-2'),
                 dbc.InputGroupText('> <'),
-                dbc.Input(readonly=True, value=round(probabilidad_mutacion, 2), style={'width': '50px'},
+                dbc.Input(readonly=True, value=round(probabilidad_mutacion, 2),
                           className='p-2')
             ], className="mr-auto p-2"),
 
             dbc.InputGroup([
                 dbc.InputGroupText("Bits Hijo 2"),
-                dbc.Input(readonly=True, value=bits_individuo_hijo2[0], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo2[0],
                           className=f'p-2 {'text-danger' if probabilidad_mutaciones_2[0] < probabilidad_mutacion else ""}'),
-                dbc.Input(readonly=True, value=bits_individuo_hijo2[1], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo2[1],
                           className=f'p-2 {'text-danger' if probabilidad_mutaciones_2[1] < probabilidad_mutacion else ""}'),
-                dbc.Input(readonly=True, value=bits_individuo_hijo2[2], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo2[2],
                           className=f'p-2 {'text-danger' if probabilidad_mutaciones_2[2] < probabilidad_mutacion else ""}'),
-                dbc.Input(readonly=True, value=bits_individuo_hijo2[3], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo2[3],
                           className=f'p-2 {'text-danger' if probabilidad_mutaciones_2[3] < probabilidad_mutacion else ""}'),
                 dbc.InputGroupText('Peso'),
-                dbc.Input(readonly=True, value=peso_hijo2, style={'width': '50px'},
+                dbc.Input(readonly=True, value=peso_hijo2,
                           className=f'p-2 {"text-success" if hijo2_vivo else "text-danger"}')
             ], className="mr-auto p-2"),
 
@@ -239,25 +237,25 @@ def card_cruce(cruce):
 
             dbc.InputGroup([
                 dbc.InputGroupText('Hijo'),
-                dbc.Input(readonly=True, value=f"{'-' if nro_hijo1 is None else nro_hijo1}", style={'width': '50px'},
+                dbc.Input(readonly=True, value=f"{'-' if nro_hijo1 is None else nro_hijo1}",
                           className=f'p-2 {"text-danger" if not hijo1_vivo else "text-success"}'),
             ], className="p-2"),
 
             dbc.InputGroup([
                 dbc.InputGroupText('Estado'),
-                dbc.Input(readonly=True, value=f"{'Vivo' if hijo1_vivo else 'Muerto'}", style={'width': '50px'},
+                dbc.Input(readonly=True, value=f"{'Vivo' if hijo1_vivo else 'Muerto'}",
                           className=f'p-2 {"text-danger" if not hijo1_vivo else "text-success"}'),
             ], className="p-2"),
 
             dbc.InputGroup([
                 dbc.InputGroupText('Bits'),
-                dbc.Input(readonly=True, value=bits_individuo_hijo1[0], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo1[0],
                           className=f'p-2 {"text-danger" if not hijo1_vivo else "text-success"}'),
-                dbc.Input(readonly=True, value=bits_individuo_hijo1[1], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo1[1],
                           className=f'p-2 {"text-danger" if not hijo1_vivo else "text-success"}'),
-                dbc.Input(readonly=True, value=bits_individuo_hijo1[2], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo1[2],
                           className=f'p-2 {"text-danger" if not hijo1_vivo else "text-success"}'),
-                dbc.Input(readonly=True, value=bits_individuo_hijo1[3], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo1[3],
                           className=f'p-2 {"text-danger" if not hijo1_vivo else "text-success"}')
             ], className="p-2")
 
@@ -267,25 +265,25 @@ def card_cruce(cruce):
 
             dbc.InputGroup([
                 dbc.InputGroupText('Hijo'),
-                dbc.Input(readonly=True, value=f"{'-' if nro_hijo2 is None else nro_hijo2}", style={'width': '50px'},
+                dbc.Input(readonly=True, value=f"{'-' if nro_hijo2 is None else nro_hijo2}",
                           className=f'p-2 {"text-danger" if not hijo2_vivo else "text-success"}'),
             ], className="p-2"),
 
             dbc.InputGroup([
                 dbc.InputGroupText('Estado'),
-                dbc.Input(readonly=True, value=f"{'Vivo' if hijo2_vivo else 'Muerto'}", style={'width': '50px'},
+                dbc.Input(readonly=True, value=f"{'Vivo' if hijo2_vivo else 'Muerto'}",
                           className=f'p-2 {"text-danger" if not hijo2_vivo else "text-success"}'),
             ], className="p-2"),
 
             dbc.InputGroup([
                 dbc.InputGroupText('Bits'),
-                dbc.Input(readonly=True, value=bits_individuo_hijo2[0], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo2[0],
                           className=f'p-2 {"text-danger" if not hijo2_vivo else "text-success"}'),
-                dbc.Input(readonly=True, value=bits_individuo_hijo2[1], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo2[1],
                           className=f'p-2 {"text-danger" if not hijo2_vivo else "text-success"}'),
-                dbc.Input(readonly=True, value=bits_individuo_hijo2[2], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo2[2],
                           className=f'p-2 {"text-danger" if not hijo2_vivo else "text-success"}'),
-                dbc.Input(readonly=True, value=bits_individuo_hijo2[3], style={'width': '50px'},
+                dbc.Input(readonly=True, value=bits_individuo_hijo2[3],
                           className=f'p-2 {"text-danger" if not hijo2_vivo else "text-success"}')
             ], className="p-2")
 
