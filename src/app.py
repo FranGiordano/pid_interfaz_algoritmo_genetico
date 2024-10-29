59b3708c574bf439227c18ceb34bfb92206110cf
Franco Giordano <francoomargiordano@hotmail.com>
2024-10-29 19:46:03 -0300

    fixs

--- src/app.py
+++ src/app.py
@@ -27,4 +27,6 @@ app.layout = html.Div([
 ])
 
 if __name__ == '__main__':
+    # from waitress import serve
+    # serve(app, host='0.0.0.0', port=80)
     app.run_server(debug=False, host="0.0.0.0", port=80)
